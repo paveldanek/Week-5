535dd8724d8e3785c006f296ae14116d9f0fdf05
paveldanek <starnet1340@yahoo.com>
2018-02-14 19:03:17 -0600

    Next script, concerning Kernel and grub.cfg. <UPDATE>

--- FourthScript.sh
+++ FourthScript.sh
@@ -8,7 +8,7 @@ echo "YOU WANNA SEE THE KERNEL PARAMETERS FROM YOUR SYSTEM'S BOOT:"
 sudo cat /proc/cmdline
 sudo cp /cdrom/boot/grub/grub.cfg ~/grub.cfg
 echo "THIS IS GRUB.CFG"
-sudo cat grub.cfg | less
+sudo cat ~/grub.cfg | less
 # I don't want to toy with grub itself, but I learned how to modify it, how to add extra menu lines to it...
 echo "CIAO..."
 
